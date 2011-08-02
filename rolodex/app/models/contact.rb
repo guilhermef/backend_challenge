ad43cec15863dc5a12d0524f92beff9b35baf087
Guilherme Souza <guilhermef@corp.globo.com>
2011-08-01 21:37:56 -0300

    house keeping v1

--- rolodex/app/models/contact.rb
+++ rolodex/app/models/contact.rb
@@ -5,8 +5,10 @@ class Contact < ActiveRecord::Base
   has_many :contact_lists
   has_many :lists, :through => :contact_lists
 
-  validates_presence_of :first_name
+  validates :first_name, :presence => true, :uniqueness => {:scope => :last_name}
   validates_presence_of :last_name
+  
+  # validates_uniqueness_of :number, :scope => [:area_code, :contact_id]
 
   # add a self to a list
   def add_to_list(list)
--- rolodex/spec/models/contact_spec.rb
+++ rolodex/spec/models/contact_spec.rb
@@ -7,7 +7,6 @@ describe Contact do
 
   it { should have_many(:phone_numbers) }
   it { should have_many(:addresses) }
-  it { should belong_to(:user) }
 
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
